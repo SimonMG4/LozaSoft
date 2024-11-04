65a65dde1b091ba5440391aaa2686861e33f34b0
SimonMG4 <simonusuario82@gmail.com>
2024-11-04 17:12:52 -0500

    backup

--- dev/views/productos.php
+++ dev/views/productos.php
@@ -14,7 +14,6 @@ if(!isset($_SESSION['id'])){
     <link rel="stylesheet" href="../../public/css/main.css">
     <link rel="stylesheet" href="../../public/css/productos.css">
     <link rel="stylesheet" href="../../public/css/modal.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
