9c105bcd3e84e940cd8b3b7e1fb4117133cabb0b
SimonMG4 <simonusuario82@gmail.com>
2024-11-30 22:30:47 -0500

    xd

--- dev/views/informes.php
+++ dev/views/informes.php
@@ -74,6 +74,8 @@ if(!isset($_SESSION['id'])){
 
     <script src="../../public/js/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="../../dev/modules/informes/informes.js"></script>
+
     
 </body>
 </html>
